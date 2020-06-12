29aaf5ce4d220765d8728eef5dedbc468e8d6658
Leidy Rosario <rosarioleidy93@gmail.com>
2020-06-12 11:19:24 +0200

    nuovo service

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,8 @@
 import { LineChartService } from './../../services/line-chart.service';
 import { PesoService } from './../../services/peso.service';
-import { Component, OnInit, Input, Output, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ModalPage } from '../modal/modal.page';
 import { ModalController, NavParams, IonFab } from '@ionic/angular';
-import { ChartDataSets } from 'chart.js';
-import { Label, Color } from 'ng2-charts';
-import * as zoomPlugin from 'chartjs-plugin-zoom';
 import { Subscription } from 'rxjs';
 
 interface Info {
@@ -21,60 +18,11 @@ interface Info {
 export class HomePage implements OnInit, OnDestroy {
   dataAttuale: Date = new Date();
   infos: Info[] = [];
-  chartData: ChartDataSets[] = [{ data: [], label: 'Peso'}];
-  chartLabels: Label[];
-  lineChartPlugins = [zoomPlugin];
-
-  // Options
-  chartOptions = {
-    responsive: true,
-    scales: {
-      xAxes: [{
-        ticks: { fontColor: 'white' },
-        gridLines: { color: 'rgba(255,255,255,0.1)' },
-        type: 'time',
-                time: {
-                    unit: 'day',
-                    displayFormats: {
-                        day: 'D'
-                    }
-                }
-      }],
-      yAxes: [{
-        ticks: { fontColor: 'white' },
-        gridLines: { color: 'rgba(255,255,255,0.1)' }
-      }]
-    },
-    plugins: {
-      zoom: {
-        pan: {
-          enabled: true,
-          mode: 'xy'
-        },
-        zoom: {
-          enabled: false,
-          mode: 'xy'
-    }
-  }
-  }
-};
-  chartsColors: Color[] = [
-    {
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
-  chartType = 'line';
-  showLegend = true;
-
   sub: Subscription;
 
   constructor(private modalController: ModalController,
-              private pesoService: PesoService) {}
+              private pesoService: PesoService,
+              public lcs: LineChartService) {}
 
   async openModal() {
     const modal = await this.modalController.create({
@@ -94,10 +42,10 @@ export class HomePage implements OnInit, OnDestroy {
         };
       });
       this.infos.sort((a, b) => new Date(a.data) > new Date(b.data) ? 1 : -1);
-      this.chartLabels = [];
+      this.lcs.chartLabels = [];
       for (const info of this.infos) {
-        this.chartLabels.push(info.data);
-        this.chartData[0].data.push(info.kg);
+        this.lcs.chartLabels.push(info.data);
+        this.lcs.chartData[0].data.push(info.kg);
       }
       console.log(this.infos);
     });
